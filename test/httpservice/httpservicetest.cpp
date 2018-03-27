58f2e658a4040ba6d50dc4fde4188a5f2bc220cb
Arthur Pachachura <pachachura.arthur@gmail.com>
2018-03-26 21:46:16 -0500

    Complete robot-side demo code

--- test/httpservice/httpservicetest.cpp
+++ test/httpservice/httpservicetest.cpp
@@ -1,62 +1,182 @@
 #include <robosub/robosub.h>
+#include <stdio.h>
+#include <stdlib.h> //rand
 
 using namespace std;
+using namespace robosub;
 
 using WsServer = robosub::ws::SocketServer<robosub::ws::WS>;
 using WsClient = robosub::ws::SocketClient<robosub::ws::WS>;
 
-int main() {
-  // WebSocket (WS)-server at port 8080 using 1 thread
-  WsServer server;
-  server.config.port = 8081;
+struct ConnectionState {
+public:
+    bool ready = false;
+    long lastSend = 0;
+    long rtt = 0;
+};
 
-  auto &echo = server.endpoint["^/echo/?$"];
+bool handleSpecialMissionControlMessage(string message) {
+    return false; //nothing to handle
+}
 
-  echo.on_message = [](shared_ptr<WsServer::Connection> connection, shared_ptr<WsServer::Message> message) {
-    auto message_str = message->string();
+void handleMissionControlState(DataBucket state) {
 
-    cout << "Server: Message received: \"" << message_str << "\" from " << connection.get() << endl;
+}
 
-    //TODO handle data from mission control
+int main(int argc, char** argv) {
+    //prepare buckets to store data
+    DataBucket current;
+    DataBucket receivedState;
 
-    // cout << "Server: Sending message \"" << message_str << "\" to " << connection.get() << endl;
-    //
-    // auto send_stream = make_shared<WsServer::SendStream>();
-    // *send_stream << message_str;
-    // // connection->send is an asynchronous function
-    // connection->send(send_stream, [](const robosub::ws::error_code &ec) {
-    //   if(ec) {
-    //     cout << "Server: Error sending message. " <<
-    //         // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
-    //         "Error: " << ec << ", error message: " << ec.message() << endl;
-    //   }
-    // });
-  };
+    //initialize server
+    WsServer server;
+    server.config.port = 8081;
+    server.config.thread_pool_size = 1;
+    server.config.address = "0.0.0.0";
 
-  echo.on_open = [](shared_ptr<WsServer::Connection> connection) {
-    cout << "Server: Opened connection " << connection.get() << endl;
-  };
+    //endpoint state storage
+    std::map<shared_ptr<WsServer::Connection>, DataBucket> connectionData;
+    std::map<shared_ptr<WsServer::Connection>, ConnectionState> connectionState;
 
-  // See RFC 6455 7.4.1. for status codes
-  echo.on_close = [](shared_ptr<WsServer::Connection> connection, int status, const string & /*reason*/) {
-    cout << "Server: Closed connection " << connection.get() << " with status code " << status << endl;
-  };
+    auto &root = server.endpoint["^/?$"];
 
-  // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
-  echo.on_error = [](shared_ptr<WsServer::Connection> connection, const robosub::ws::error_code &ec) {
-    cout << "Server: Error in connection " << connection.get() << ". "
-         << "Error: " << ec << ", error message: " << ec.message() << endl;
-  };
+    root.on_open = [&connectionData, &connectionState, &current](shared_ptr<WsServer::Connection> connection) {
+        cout << "Server: Opened connection " << connection.get() << endl;
 
-  thread server_thread([&server]() {
-    // Start WS-server
-    server.start();
-  });
+        //put current state into state storage
+        connectionData[connection] = current;
+        connectionState[connection] = ConnectionState();
+        connectionState[connection].lastSend = robosub::Time::millis();
 
-  // Wait for server to start so that the client can connect
-  this_thread::sleep_for(chrono::seconds(1));
+        //convert current state to stream
+        auto send_stream = make_shared<WsServer::SendStream>();
+        *send_stream << connectionData[connection];
 
-  cout << "Started robot service demo" << endl;
+        //send current state
+        connection->send(send_stream, [](const robosub::ws::error_code &ec) {
+            if(ec) {
+                cout << "Server: Error sending message. " <<
+                     // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
+                     "Error: " << ec << ", error message: " << ec.message() << endl;
+            }
+        });
+    };
 
-  server_thread.join();
+    root.on_message = [&connectionState,&receivedState](shared_ptr<WsServer::Connection> connection, shared_ptr<WsServer::Message> message) {
+        auto message_str = message->string();
+
+        if (message_str == "\x06") {
+            connectionState[connection].ready = true;
+            connectionState[connection].rtt = robosub::Time::millis() - connectionState[connection].lastSend;
+        } else {
+            cout << "Server: Message received: " << message_str << endl;
+
+            //handle messages from mission control
+            if (!handleSpecialMissionControlMessage(message_str)) {
+
+                char firstCharacter = message_str.at(0);
+                if (firstCharacter == '[') {
+                    //data is compressed - inflate
+                    DataBucket received = DataBucket(message_str);
+                    receivedState = received.inflate(receivedState);
+                } else {
+                    //data bucket is not compressed
+                    receivedState = DataBucket(message_str);
+                }
+
+                handleMissionControlState(receivedState);
+            }
+
+            //send ACK
+            auto send_stream = make_shared<WsServer::SendStream>();
+            *send_stream << "\x06"; //ACK
+            connection->send(send_stream, [](const robosub::ws::error_code &ec) {
+                if(ec) {
+                    cout << "Server: Error sending message. " <<
+                         // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
+                         "Error: " << ec << ", error message: " << ec.message() << endl;
+                } else {
+                    cout << "Server: Send ACK" << endl;
+                }
+            });
+        }
+    };
+
+    // See RFC 6455 7.4.1. for status codes
+    root.on_close = [&connectionState, &connectionData](shared_ptr<WsServer::Connection> connection, int status, const string & /*reason*/) {
+        connectionState.erase(connection);
+        connectionData.erase(connection);
+        cout << "Server: Closed connection " << connection.get() << " with status code " << status << endl;
+    };
+
+    // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
+    root.on_error = [&connectionState, &connectionData](shared_ptr<WsServer::Connection> connection, const robosub::ws::error_code &ec) {
+        connectionState.erase(connection);
+        connectionData.erase(connection);
+        cout << "Server: Error in connection " << connection.get() << ". "
+             << "Error: " << ec << ", error message: " << ec.message() << endl;
+    };
+
+    thread server_thread([&server]() {
+        // Start WS-server
+        server.start();
+    });
+
+    // Wait for server to start so that the client can connect
+    robosub::Time::waitMillis(1000);
+    cout << "Server started" << endl;
+
+    int i = 0;
+
+    while(true) {
+        current["index"] = i++ % 1000; //force refresh approx every second
+        current["rand"] = rand() % 100;
+
+        robosub::Time::waitMillis(100);
+        unsigned long milliseconds_since_epoch = robosub::Time::millis();
+
+        //send data to connections
+        for(auto &connection : server.get_connections())
+        {
+            if (!connectionState[connection].ready) continue;
+
+            //compress data
+            DataBucket previousState = connectionData[connection];
+            DataBucket compressed = current.compress(previousState);
+
+            //skip if no changes
+            if (compressed.toJson().empty()) continue;
+
+            //update time-dependent values and recompress
+            DataBucket sentState = current;
+            sentState["time"] = milliseconds_since_epoch;
+            sentState["rtt"] = connectionState[connection].rtt;
+            compressed = sentState.compress(previousState);
+
+            //set new connection state
+            connectionData[connection] = current;
+
+            //update connection state
+            connectionState[connection].ready = false;
+            connectionState[connection].lastSend = robosub::Time::millis();
+
+            //check if better to send as compressed or uncompressed
+            //cout << current.toString().length() << " " << compressed.toString().length() << endl;
+            if (current.toString().length() < compressed.toString().length())
+            {
+                //better to send uncompressed
+                auto send_stream = make_shared<WsServer::SendStream>();
+                *send_stream << sentState;
+                connection->send(send_stream);
+            } else {
+                //send compressed
+                auto send_stream = make_shared<WsServer::SendStream>();
+                *send_stream << compressed;
+                connection->send(send_stream);
+            }
+
+        }
+    }
+
+    server_thread.join();
 }
