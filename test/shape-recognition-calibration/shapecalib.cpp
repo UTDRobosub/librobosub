aa7199bd80dbb49eff744c0334fac533d655b935
Lauren :) <laurenbarbeau2@gmail.com>
2019-11-13 19:15:38 -0600

    added shape recognition

---
+++ test/shape-recognition-calibration/shapecalib.cpp
@@ -0,0 +1,15 @@
+//
+// Created by robosub on 11/13/19.
+//
+
+#include "shapecalib.h"
+
+using namespace std;
+
+namespace robosub {
+    int main() {
+
+    }
+
+
+}
---
+++ test/shape-recognition-calibration/shapecalib.h
@@ -0,0 +1,14 @@
+//
+// Created by robosub on 11/13/19.
+//
+
+#ifndef LIBROBOSUB_SHAPECALIB_H
+#define LIBROBOSUB_SHAPECALIB_H
+
+
+class shapecalib {
+
+};
+
+
+#endif //LIBROBOSUB_SHAPECALIB_H
